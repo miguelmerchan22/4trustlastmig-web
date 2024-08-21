426cd44d9bca4fb0b6304e7fcafa96de34e4f50b
miguelmerchan22 <miguelmerchan@hotmail.fr>
2024-08-20 18:44:53 -0600

    1

---
+++ precache-manifest.174813ecd1be28afeb4118db37132802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06572f3168d9e5ccc0e0a7c488900e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73ca1b492c89b2cd585",
+    "url": "/static/js/2.836a776e.chunk.js"
+  },
+  {
+    "revision": "741da2da7ddac13e2716a2a97e471295",
+    "url": "/static/js/2.836a776e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac9e690f543bed5afa1",
+    "url": "/static/js/main.4c159bb4.chunk.js"
+  },
+  {
+    "revision": "1a97896142c7e53f9cda",
+    "url": "/static/js/runtime-main.6447b523.js"
+  }
+]);
